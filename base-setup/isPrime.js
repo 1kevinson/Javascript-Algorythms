30d742f1e34908b87e189c68e12c9c0fa7799077
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-03 12:13:51 +0200

    isPrime algorithm

---
+++ base-setup/isPrime.js
@@ -0,0 +1,48 @@
+/**
+ * @ISPRIME Check if number entered is prime number
+ */
+
+// First solution
+function isPrime(number) {
+  let result = false;
+  const arrayOfResult = [];
+
+  for (let i = 1; i <= number; i++) {
+    let divider = number / i;
+    if (Number.isInteger(divider)) {
+      arrayOfResult.push("int");
+    } else {
+      arrayOfResult.push("notInt");
+    }
+  }
+
+  const filterResult = arrayOfResult.filter((word) => word === "int");
+  if (filterResult.length === 2) {
+    result = true;
+  }
+
+  return result;
+}
+
+// Second Solution
+function isPrime2(number) {
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let start = performance.now();
+isPrime2(37); // Take lower time
+let end = performance.now();
+console.log(end - start);
+
+start = performance.now();
+isPrime(37); // Take more time, because of more loop and built in function
+end = performance.now();
+console.log(end - start);
+
+console.log(isPrime(13)); // true
+console.log(isPrime(22)); // false
