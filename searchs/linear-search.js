1bdb5651678514cca6473567337e32a42d6f6b1a
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-06 17:40:20 +0200

    Linear time complexity

--- searchs/linear-search.js
+++ searchs/linear-search.js
@@ -1,9 +1,18 @@
-function findElement(arr, seekElement) {
+function findElement(arr, seekElement, comparatorFn) {
   let index = 0;
 
   for (const element of arr) {
-    // noinspection EqualityComparisonWithCoercionJS
-    if (element == seekElement) {
+    //Object case
+    if (
+      typeof seekElement === "object" &&
+      seekElement !== null &&
+      comparatorFn(seekElement, element)
+    ) {
+      return index;
+    }
+
+    //Array case
+    if (element === seekElement) {
       return index;
     }
     index++;
@@ -11,4 +20,17 @@ function findElement(arr, seekElement) {
 }
 
 const array = [2, 14, 5, 66, 47, 88, 32, 9];
+const objects = [
+  { name: "Martin", age: 25 },
+  { name: "Sabrine", age: 23 },
+  { name: "Kevin", age: 26 },
+];
+
 console.log(findElement(array, 66)); // should return 3
+console.log(findElement(array, 32)); // should return 6
+
+console.log(
+  findElement(objects, { name: "Martin", age: 25 }, (x, y) => {
+    return x.name === y.name;
+  })
+);
