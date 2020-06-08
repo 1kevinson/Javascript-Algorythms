ba21b15ce32daef1d22c4071a26ea3f6e44b7b4b
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-08 14:57:53 +0200

    cartesian product algo

--- Sets (Array)/cartesian-product.js
+++ Sets (Array)/cartesian-product.js
@@ -3,8 +3,18 @@
  *
  */
 
-function cartProduct(setA, setB) {}
+function cartProduct(setA, setB) {
+  const product = [];
 
-const colors = ["blue", "red"];
-const sizes = ["m", "l"];
+  for (const setAEl of setA) {
+    for (const setBEl of setB) {
+      product.push([setAEl, setBEl]);
+    }
+  }
+
+  return product;
+}
+
+const colors = ["blue", "red", "black"];
+const sizes = ["s", "m", "l"];
 console.log(cartProduct(colors, sizes));
